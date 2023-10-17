7445d80a02116ebecb524d56188f13b50643d518
JeasonZuo <jeasonzuo@163.com>
2023-10-18 02:34:04 +0800

    element plus

--- src/components/WebSocketChat.vue
+++ src/components/WebSocketChat.vue
@@ -5,41 +5,38 @@
       <div v-for="(message, index) in messages" :key="index">
         {{ message }}
       </div>
-      <input v-model="newMessage" @keyup.enter="handleSendMessage" />
-      <button @click="handleSendMessage">发送消息</button>
-      <button @click="disconnectWebSocket">断开连接</button>
+      <el-input
+          v-model="newMessage"
+          @keyup.enter="handleSendMessage"
+          size="default"
+          placeholder="Please Input"
+          style="width: 300px; margin:3px"
+      />
+      <el-button type="success" @click="handleSendMessage">发送消息</el-button>
+      <el-button type="danger"  @click="disconnectWebSocket">断开连接</el-button>
     </div>
     <div v-else>
       <p>WebSocket is not connected.</p>
-      <button @click="connectWebSocket">开启连接</button>
+      <el-button type="success" @click="connectWebSocket">开启连接</el-button>
     </div>
   </div>
 </template>
 
-<script>
+<script setup>
 import { ref } from 'vue';
 import { useWebSocket } from '../api/WebSocketService';
 
-export default {
-  setup() {
-    const { messages, isConnected, sendMessage, connectWebSocket, disconnectWebSocket  } = useWebSocket();
-    const newMessage = ref('');
 
-    const handleSendMessage = () => {
-      if (newMessage.value.trim() !== '') {
-        sendMessage({ message: newMessage.value });
-        newMessage.value = '';
-      }
-    };
+const { messages, isConnected, sendMessage, connectWebSocket, disconnectWebSocket  } = useWebSocket();
+const newMessage = ref('');
 
-    return {
-      messages,
-      isConnected,
-      newMessage,
-      handleSendMessage,
-      connectWebSocket,
-      disconnectWebSocket,
-    };
-  },
+const handleSendMessage = () => {
+  if (newMessage.value.trim() !== '') {
+    sendMessage({ message: newMessage.value });
+    newMessage.value = '';
+  }
 };
 </script>
+
+<style>
+</style>
