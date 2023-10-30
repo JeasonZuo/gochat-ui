b32bac88063a0a802940abb3d8f704a8ce59ecb4
JeasonZuo <jeasonzuo@163.com>
2023-10-30 10:22:16 +0800

    update deploy shell

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ docker build . -t tt-chat-fe-img
 docker stop tt-chat-frontend > /dev/null 2>&1
 docker rm tt-chat-frontend > /dev/null 2>&1
 
-docker run --name tt-chat-frontend -d -p 80:80  tt-chat-fe-img
+docker run -d -p 80:80 --restart=always --name tt-chat-frontend tt-chat-fe-img
